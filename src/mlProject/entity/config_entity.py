719eb7476ddac2880305a456511413ce3f3676fb
klan86at <klang86at@gmail.com>
2025-07-01 15:29:47 +0300

    Data ingestion added

---
+++ src/mlProject/entity/config_entity.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_url: str
+    local_data_file: Path
+    unzip_dir: Path
+
+    # def __post_init__(self):
+    #     # Ensure that the paths are absolute
+    #     self.root_dir = Path(self.root_dir).resolve()
+    #     self.local_data_file = Path(self.local_data_file).resolve()
+    #     self.unzip_dir = Path(self.unzip_dir).resolve()
